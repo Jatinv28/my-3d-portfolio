1d28f7015df2fd02a67e15fa74f4d13ae94beda9
Jatinv28 <jatinverma96225@gmail.com>
2026-03-22 23:27:45 +0530

    Update Landing and Loading components

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -17,12 +17,12 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-info">
             <h3> JATIn VERMA </h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">STUDENT</div>
+              <div className="landing-h2-1">DEVELOPER</div>
               <div className="landing-h2-2">Engineer</div>
             </h2>
             <h2>
               <div className="landing-h2-info">Engineer</div>
-              <div className="landing-h2-info-1">STUDENT</div>
+              <div className="landing-h2-info-1">DEVELOPER</div>
             </h2>
           </div>
         </div>
